7ba88463e9512d490667c77c6e0f6386a00a53d8
Kaniz Ayesha <kanizayesha1122@gmail.com>
2026-07-11 22:46:10 +0500

    Added Python basics

---
+++ basic.py
@@ -0,0 +1,15 @@
+print("hello world")
+print(1+2)
+print(29*20)
+name="Kaniz Ayesha"
+print(name)
+print(name.upper())
+print(name.lower())
+x=20
+y=33
+z=12
+print(x+y+z)
+print  ("My name is",name,"and I am",x,"years old")
+print("My name is {} and I am {} years old".format(name,x))
+print(f"My name is {name} and I am {x} years old")
+print(x-y)
