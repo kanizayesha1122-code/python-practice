c49fc37231c5c456e95e3a72f20c9464750527b9
Kaniz Ayesha <kanizayesha1122@gmail.com>
2026-07-15 21:59:11 +0500

    variable book exercise

---
+++ variables exercise.py
@@ -0,0 +1,25 @@
+"""
+===========================================
+        PYTHON CRASH COURSE EXERCISES
+===========================================
+
+📚 Book: Python Crash Course (3rd Edition)
+👨‍💻 Chapter: Variables and Simple Data Types
+📝 File: variables_exercises.py
+🎯 Purpose: Solutions to the "Try It Yourself" exercises.
+
+Author: Kaniz Ayesha
+Date Started: July 15, 2026
+
+===========================================
+"""
+
+'''TRY IT YOURSELF EXERCISES'''
+"""===CHP 2 ,PG :23==="""
+name='kaniz ayesha'
+print(name)
+message='hello world'
+print(message)
+message="my nam is ayesha"
+print(message)
+ 
